e0c76cd3ec723b35e60a952dc5389687c34658a7
pedrolemosbonini <pedrolemosbonini@gmail.com>
2025-04-07 13:54:53 -0300

    Procedures & Functions MYSQL

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:"10.89.234.148",
+    host:"10.89.240.71",
     user:"alunods",
     password:"senai@604",
     database:"vio_bonini"
